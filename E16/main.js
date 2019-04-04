ef9ac8b0bf811b06dd6a9a3da53a7365cf43e0cf
Lauren <Mike@Mikes-MacBook-Pro.local>
2019-04-04 09:44:00 -0400

    k

---
+++ E16/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  $('#number1').click(function() {
+    $('#number1').css('color', 'yellow');
+    $('color1').css('background-color','black');
+    $('color1').toggleClass('black');
+  });
+});
