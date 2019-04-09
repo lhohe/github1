cb1be059a78682ce899ab8f33f0e68a2376ffb68
Lauren <Mike@Mikes-MacBook-Pro.local>
2019-04-09 09:41:44 -0400

    e16

--- E16/main.js
+++ E16/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  // $('h1').css('color', 'white');
   $('#number5').click(function() {
     $('#number5').css('color', 'white');
     $('#bird-5').toggleClass('black');
