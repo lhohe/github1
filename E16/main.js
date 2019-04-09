801f0a257c2497620ba4c988f828de5e941b5252
Lauren <Mike@Mikes-MacBook-Pro.local>
2019-04-09 10:11:45 -0400

    e16

--- E16/main.js
+++ E16/main.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
-  $('#number5').click(function() {
-    $('#number5').css('color', 'white');
-    $('#bird-5').toggleClass('black');
+  $('#color-1').click(function() {
+    $('#color-1').css('color', 'orange');
+    $('#color-1').toggleClass('black');
   });
   $('#number4').click(function() {
     $('#number4').css('color', 'goldenrod');
   });
+});
+
+$('p').addClass('bigger');
+console.log('added bigger');
+$('p').click(function(){
+  $('p').css('color', 'white');
+  // added a message to the console, helpful in debugging
+  console.log('added white');
+  $('body').toggleClass('bodyBG');
 });
