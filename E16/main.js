dd61594fbc10e69e774f41db921af46fb068cc7f
Lauren <Mike@Mikes-MBP.byod.gmu.edu>
2019-04-09 12:45:35 -0400

    e16

--- E16/main.js
+++ E16/main.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-  $('#color-1').click(function() {
-    $('#color-1').css('color', 'orange');
-    $('#color-1').toggleClass('black');
+  $('#number1').click(function() {
+    $('#color-1').toggleClass('b1');
+  });
+  $('#number2').click(function() {
+    $('#color-2').toggleClass('b2');
+  });
+  $('#number3').click(function() {
+    $('#color-3').toggleClass('b3');
   });
   $('#number4').click(function() {
-    $('#number4').css('color', 'goldenrod');
+    $('#color-4').toggleClass('b4');
   });
-});
+  $('#number5').click(function() {
+    $('#color-5').toggleClass('b5');
+  });
 
-$('p').addClass('bigger');
-console.log('added bigger');
-$('p').click(function(){
-  $('p').css('color', 'white');
-  // added a message to the console, helpful in debugging
-  console.log('added white');
-  $('body').toggleClass('bodyBG');
 });
