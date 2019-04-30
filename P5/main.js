87d32f664227163856ffe13f6fb7626208c70afa
Lauren <Mike@Mikes-MacBook-Pro.local>
2019-04-30 10:11:40 -0400

    mn

--- P5/main.js
+++ P5/main.js
@@ -1,11 +1,35 @@
 $(document).ready(function() {
-  $('#pink').click(function() {
-    $('#pink').css('color', 'white');
-    $('#pink').toggleClass('orange');
+  $('#1').click(function() {
+    $('#color-1').toggleClass('b1')
+    $('h2').text('Orange is my favorite color too!');
   });
-  $('#red').click(function() {
-    $('#number4').css('color', 'goldenrod');
-    $('#red').toggleClass('text-color-to-white');
-    console.log('color change')
+  $('#2').click(function() {
+    $('#color-2').toggleClass('b1')
+    $('h2').text('Orange is my favorite color too!');
   });
+  $('#3').click(function() {
+    $('#color-3').toggleClass('b1')
+    $('h2').text('Yay we have the same favorite color!');
+  });
+  $('#4').click(function() {
+    $('#color-4').toggleClass('b1')
+    $('h2').text('Orange is my favorite color too!');
+  });
+  $('#5').click(function() {
+    $('#color-5').toggleClass('b1')
+    $('h2').text('Orange is my favorite color too!');
+  });
+  $('#6').click(function() {
+    $('#color-6').toggleClass('b1')
+    $('h2').text('Orange is my favorite color too!');
+  });
+  $('#7').click(function() {
+    $('#color-7').toggleClass('b1')
+    $('h2').text('Orange is my favorite color too!');
+  });
+  $('#8').click(function() {
+    $('#color-8').toggleClass('b1')
+    $('h2').text('Orange is my favorite color too!');
+  });
+
 });
